9493edbe9d744ff0ea22167f83f5774d06a0ca51
Stephen Downey <downeys07@gmail.com>
2021-03-25 11:27:17 -0500

    adds behavior to customer dao impl

--- U1M6GroupProject/src/main/java/com/company/U1M6GroupProject/dao/CustomerDaoJdbcTemplateImpl.java
+++ U1M6GroupProject/src/main/java/com/company/U1M6GroupProject/dao/CustomerDaoJdbcTemplateImpl.java
@@ -2,13 +2,29 @@ package com.company.U1M6GroupProject.dao;
 
 import com.company.U1M6GroupProject.models.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
 public class CustomerDaoJdbcTemplateImpl implements CustomerDao{
+
+    private static final String INSERT_CUSTOMER_SQL =
+            "insert into customer (first_name, last_name, email, company, phone) values (?, ?, ?, ?, ?)";
+    private static final String SELECT_CUSTOMER_SQL =
+            "select * from customer where customer_id = ?";
+    private static final String SELECT_ALL_CUSTOMER_SQL =
+            "select * from customer";
+    private static final String UPDATE_CUSTOMER_SQL =
+            "update customer set first_name = ?, last_name = ?, email = ?, company = ?, phone = ? " +
+                    "where customer_id = ?";
+    private static final String DELETE_CUSTOMER_SQL =
+            "delete from customer where customer_id = ?";
 
     private JdbcTemplate jdbcTemplate;
 
@@ -19,26 +35,63 @@ public class CustomerDaoJdbcTemplateImpl implements CustomerDao{
 
     @Override
     public Customer addCustomer(Customer customer) {
-        return null;
+        //use Insert prepared statement to update database (add customer to database)
+        jdbcTemplate.update(INSERT_CUSTOMER_SQL, customer.getFirstName(), customer.getLastName(),
+                customer.getEmail(), customer.getCompany(), customer.getPhone());
+        //get the id that was assigned to the customer
+        int id = jdbcTemplate.queryForObject("select LAST_INSERT_ID()", Integer.class);
+        //set the customer id
+        customer.setCustomerId(id);
+        //return completed object
+        return customer;
     }
 
     @Override
     public Customer getCustomer(int id) {
-        return null;
+        //queryForObject throws an exception, so we have to catch it
+        try{
+            //use Select prepared statement to retrieve customer from database by its id
+            return jdbcTemplate.queryForObject(SELECT_CUSTOMER_SQL, this::mapRowToCustomer, id);
+        }catch (EmptyResultDataAccessException e){
+            //if there isn't a record in the table with that id, return null
+            return null;
+        }
     }
 
     @Override
     public List<Customer> getAllCustomers() {
-        return null;
+        //use Select_All prepared statement to retrieve all Customer tuples from table
+        return jdbcTemplate.query(SELECT_ALL_CUSTOMER_SQL, this::mapRowToCustomer);
     }
 
     @Override
     public void updateCustomer(Customer customer) {
-
+        //use Update prepared statement to update the customer tuple
+        jdbcTemplate.update(UPDATE_CUSTOMER_SQL,
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getEmail(),
+                customer.getCompany(),
+                customer.getPhone(),
+                customer.getCustomerId());
     }
 
     @Override
     public void deleteCustomer(int id) {
+        //use Delete prepared statement to delete customer tuple
+        jdbcTemplate.update(DELETE_CUSTOMER_SQL, id);
+    }
 
+    private Customer mapRowToCustomer(ResultSet rs, int rowNum) throws SQLException {
+        //create a customer object to parse the returned Json string
+        Customer customer = new Customer();
+        //Assign each value pair from the ResultSet to its corresponding Customer member value
+        customer.setCustomerId(rs.getInt("customer_id"));
+        customer.setFirstName(rs.getString("first_name"));
+        customer.setLastName(rs.getString("last_name"));
+        customer.setPhone(rs.getString("phone"));
+        customer.setEmail(rs.getString("email"));
+        customer.setCompany(rs.getString("company"));
+        return customer;
     }
 }
--- U1M6GroupProject/src/test/java/com/company/U1M6GroupProject/dao/CustomerDaoJdbcTemplateImplTest.java
+++ U1M6GroupProject/src/test/java/com/company/U1M6GroupProject/dao/CustomerDaoJdbcTemplateImplTest.java
@@ -56,13 +56,104 @@ public class CustomerDaoJdbcTemplateImplTest {
 
     @Test
     public void getAllCustomers() {
+        //arrange
+        Customer customer1 = new Customer();
+        customer1.setFirstName("Jerry");
+        customer1.setLastName("Garcia");
+        customer1.setEmail("jgarcia@gmail.com");
+        customer1.setCompany("Grateful Dead");
+        customer1.setPhone("2813846385");
+
+        Customer customer2 = new Customer();
+        customer2.setFirstName("John");
+        customer2.setLastName("Cena");
+        customer2.setEmail("jcena@gmail.com");
+        customer2.setCompany("WWE");
+        customer2.setPhone("8196543546");
+
+        customerDao.addCustomer(customer1);
+        customerDao.addCustomer(customer2);
+
+        //Act
+        List<Customer> customersReceivedFromDatabase = customerDao.getAllCustomers();
+
+        //Assert
+        assertEquals(2, customersReceivedFromDatabase.size());
+
     }
 
     @Test
     public void updateCustomer() {
+        //arrange
+        Customer customer1 = new Customer();
+        customer1.setFirstName("Jerry");
+        customer1.setLastName("Garcia");
+        customer1.setEmail("jgarcia@gmail.com");
+        customer1.setCompany("Grateful Dead");
+        customer1.setPhone("2813846385");
+
+        customer1 = customerDao.addCustomer(customer1);
+        Customer updatedCustomer = customerDao.getCustomer(customer1.getCustomerId());
+        updatedCustomer.setFirstName("Geddy");
+        updatedCustomer.setLastName("Lee");
+        updatedCustomer.setCompany("Rush");
+        updatedCustomer.setPhone("45678932157");
+        updatedCustomer.setEmail("geddy@rush.com");
+
+        //Act
+        customerDao.updateCustomer(updatedCustomer);
+        Customer customerReceivedFromDatabase = customerDao.getCustomer(customer1.getCustomerId());
+
+        //Assert
+        assertEquals(updatedCustomer, customerReceivedFromDatabase);
+        assertNotEquals(customer1, customerReceivedFromDatabase);
+
     }
 
     @Test
     public void deleteCustomer() {
+        //arrange
+        //Create objects to add to the database
+        Customer customer1 = new Customer();
+        customer1.setFirstName("Jerry");
+        customer1.setLastName("Garcia");
+        customer1.setEmail("jgarcia@gmail.com");
+        customer1.setCompany("Grateful Dead");
+        customer1.setPhone("2813846385");
+
+        Customer customer2 = new Customer();
+        customer2.setFirstName("John");
+        customer2.setLastName("Cena");
+        customer2.setEmail("jcena@gmail.com");
+        customer2.setCompany("WWE");
+        customer2.setPhone("8196543546");
+
+        //add the created objects to the database
+        customer1 = customerDao.addCustomer(customer1);
+        customer2 = customerDao.addCustomer(customer2);
+
+        //First get all of the customers from the database to confirm the original list size
+        List<Customer> customersReceivedFromDatabase = customerDao.getAllCustomers();
+        //confirms original listSize should be 2 because we added two customers
+        assertEquals(2, customersReceivedFromDatabase.size());
+
+        //Act
+        //delete one customer from the database, get all customers from database again, and confirm new list size
+        customerDao.deleteCustomer(customer1.getCustomerId());
+        customersReceivedFromDatabase = customerDao.getAllCustomers();
+
+        //Assert
+        //list size should now equal 1 because we deleted a customer
+        assertEquals(1, customersReceivedFromDatabase.size());
+
+        //Act
+        //delete the other customer and check the list size one more time
+        customerDao.deleteCustomer(customer2.getCustomerId());
+        customersReceivedFromDatabase = customerDao.getAllCustomers();
+
+        //Assert
+        //list size should now equal 0 because we deleted the last customer
+        assertEquals(0, customersReceivedFromDatabase.size());
+
     }
 }
